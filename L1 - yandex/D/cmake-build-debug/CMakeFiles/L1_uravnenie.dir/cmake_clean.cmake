2859bf6bee2cf1623b8e08eebe1dda5c309639d9
DeynegaEkaterina <deynega.katena@mail.ru>
2021-06-15 14:34:02 +0300

    refresh

---
+++ L1 - yandex/D/cmake-build-debug/CMakeFiles/L1_uravnenie.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/L1_uravnenie.dir/main.cpp.o"
+  "L1_uravnenie"
+  "L1_uravnenie.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/L1_uravnenie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
