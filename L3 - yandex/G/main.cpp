34d9ca77643ade8a619c18abe64e024ba42a38ea
DeynegaEkaterina <deynega.katena@mail.ru>
2021-06-16 17:11:38 +0300

    failed test 10

--- L3 - yandex/G/main.cpp
+++ L3 - yandex/G/main.cpp
@@ -1,47 +1,29 @@
 #include <iostream>
-#include "vector"
-#include "algorithm"
+#include "utility"
+#include "set"
 
+//не проходит тест 10, нужно сделать чтение данных из файла
+using namespace std;
 
-struct para{
-    int a;
-    int b;
-};
-
-int turtles(int n, std::vector<para> a){
-int count = 0;
-for (int i = 0; i < a.size(); i ++){
-    if(a[i].a + a[i].b == (n - 1)){
-        count++;
-    }
-}
-    return count;
-}
-
-
-bool compare(para a, para b){
-    if (a.a == b.a && a.b == b.b){
-        return true;
-    } else return false;
-}
 
 int main() {
+    pair<int, int> turtle;
     int n;
-    std::cin >> n;
-    if ((n > 10000) || (n < 1)) {exit(0);}
-    para c;
-    std::vector<para> va;
-
-    for(int i=0; i < n; i++){
-        std::cin >> c.a >> c.b;
-        if (c.a > 10000  || c.a < 0 || c.b < 0 || c.b > 10000) { exit(0);}
-        va.push_back(c);
+    cin >> n;
+    if(n < 1 || n > 10000){ exit(0);}
+    set<pair<int, int>> s;
+    for(int i = 0; i != n; i++){
+        cin >> turtle.first >> turtle.second;
+        if(turtle.first >=0 && turtle.second >= 0) {
+            s.insert(turtle);
+        }
     }
-
-    std::vector<para>::iterator ip;
-    ip = std::unique(va.begin(), va.end(), compare);
-    va.resize(std::distance(va.begin(), ip));
-
-    std::cout << turtles(n, va);
+    int count;
+    for(const pair<int,int>& it : s){
+      if(it.first + it.second == n - 1){
+          count++;
+      }
+    }
+    std::cout << count;
 }
 
