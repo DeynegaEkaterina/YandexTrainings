fba1056e023eba30a384a205ac47c66db43d7974
DeynegaEkaterina <deynega.katena@mail.ru>
2021-06-16 18:40:33 +0300

    done

--- L3 - yandex/G/main.cpp
+++ L3 - yandex/G/main.cpp
@@ -2,9 +2,7 @@
 #include "utility"
 #include "set"
 
-//не проходит тест 10, нужно сделать чтение данных из файла
 using namespace std;
-
 
 int main() {
     pair<int, int> turtle;
@@ -14,16 +12,12 @@ int main() {
     set<pair<int, int>> s;
     for(int i = 0; i != n; i++){
         cin >> turtle.first >> turtle.second;
-        if(turtle.first >=0 && turtle.second >= 0) {
-            s.insert(turtle);
+        if(turtle.first >= 0 && turtle.second >= 0 && turtle.first+turtle.second+1 == n) {
+           s.insert(turtle);
+        } else {
+            continue;
         }
     }
-    int count;
-    for(const pair<int,int>& it : s){
-      if(it.first + it.second == n - 1){
-          count++;
-      }
-    }
-    std::cout << count;
+    std::cout << s.size();
 }
 
