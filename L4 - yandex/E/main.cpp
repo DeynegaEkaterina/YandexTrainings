e886e321f8a0f648a15592ca21e5efe71d816920
DeynegaEkaterina <deynega.katena@mail.ru>
2021-06-21 23:59:26 +0300

    unsolved

---
+++ L4 - yandex/E/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "map"
+
+using namespace std;
+int main() {
+
+    map<int,int> blocks;
+    int n;
+    int a, b;
+    cin >> n;
+    if(n < 1 || n >100000) {exit(0);}
+    for(int i = 0; i < n; i++){
+        cin >> a >> b;
+        if(a < 1 || a >1000000000 || b < 1 || b > 1000000000) {exit(0);}
+        if (blocks.contains(a)){
+            blocks[a] = max(b, blocks[a]);
+        }
+        blocks.insert(make_pair(a,b));
+    }
+
+    int maxHeight = 0;
+    for (const auto& [key, value] : blocks){
+        maxHeight += value;
+    }
+    cout << maxHeight;
+    return 0;
+}
